4ce06899ba5ddd4c3c6fd77b9bcfe5ec0c6d8f4a
valenkita <jessicavaleriah@gmail.com>
2022-09-11 15:16:09 +0000

    Creating loop for jumping, adding clouds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 
-function jump () {
+const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
@@ -8,7 +10,36 @@ function jump () {
     }, 500)
 }
 
-const loop = setInterval
+const loop = setInterval(() => {
+
+    console.log('loop')
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    const cloudsPosition = clouds.offsetLeft;
+
+    console.log(marioPosition);
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './assets/images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
+        clearInterval(loop);
+
+
+    }
+}, 10);
 
 document.addEventListener('keydown', jump)
 
