9b1d1cabfee768e76be1620dfe6ba65c587f0087
valenkita <jessicavaleriah@gmail.com>
2022-09-12 18:56:30 +0000

    adding ground to the game, adding buttons 'play' and 'instructions', adding text for score, lives and time'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
+const ground = document.querySelector('.ground');
 
+//number of lives
+var currentLife = 3;
+
+
+
+// mario jumping 
 const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
         mario.classList.remove('jump');
-    }, 500)
+    }, 400)
 }
 
 const loop = setInterval(() => {
@@ -17,10 +24,11 @@ const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
     const cloudsPosition = clouds.offsetLeft;
+    const groundPosition = ground.offsetLeft;
 
     console.log(marioPosition);
 
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 145) {
         
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
@@ -35,11 +43,21 @@ const loop = setInterval(() => {
         clouds.style.animation = 'none';
         clouds.style.left = `${cloudsPosition}px`;
 
+        ground.style.animation = 'none';
+        ground.style.left = `${groundPosition}px`;
+
         clearInterval(loop);
-
-
     }
 }, 10);
 
 document.addEventListener('keydown', jump)
+
+function life() {
+    starElementsArray[currentLife].style.opacity = 0.1;
+    currentLife--;
+    if(currentLife < 0){
+        console.log(GameOver);
+    }
+
+}
 
